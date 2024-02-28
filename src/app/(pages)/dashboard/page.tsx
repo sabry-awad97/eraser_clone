aae54ec1993342ce5a71cbc156f3bcfeabc16435
Sabry Awad <dr.sabry97@gmail.com>
2024-02-29 00:06:28 +0200

    🎉 feat(dashboard/page.tsx): Implement useUserCreation hook to create users if they do not exist 🎉 feat(useUserCreation.tsx): Add useUserCreation hook for user creation functionality

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,41 +1,11 @@
 'use client';
 
+import useUserCreation from '@/app/_hooks/useUserCreation';
 import { Button } from '@/components/ui/button';
-import { api } from '@/convex/_generated/api';
-import {
-  LogoutLink,
-  useKindeBrowserClient,
-} from '@kinde-oss/kinde-auth-nextjs';
-import { useConvex } from 'convex/react';
-import { useEffect } from 'react';
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs';
 
 const Dashboard = () => {
-  const { user } = useKindeBrowserClient();
-  const convex = useConvex();
-
-  useEffect(() => {
-    const checkAndCreateUserIfNotExist = async () => {
-      try {
-        if (!user?.email) return;
-
-        const users = await convex.query(api.user.getUsers, {
-          email: user.email,
-        });
-
-        if (users.length === 0) {
-          await convex.mutation(api.user.createUser, {
-            name: user.given_name ?? '',
-            email: user.email,
-            image: user.picture ?? '',
-          });
-        }
-      } catch (error) {
-        console.error('Error while checking user:', error);
-      }
-    };
-
-    checkAndCreateUserIfNotExist();
-  }, [convex, user]);
+  useUserCreation();
 
   return (
     <div>
---
+++ src/app/_hooks/useUserCreation.tsx
@@ -0,0 +1,35 @@
+import { api } from '@/convex/_generated/api';
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import { useConvex } from 'convex/react';
+import { useEffect } from 'react';
+
+const useUserCreation = () => {
+  const { user } = useKindeBrowserClient();
+  const convex = useConvex();
+
+  useEffect(() => {
+    const checkAndCreateUserIfNotExist = async () => {
+      try {
+        if (!user?.email) return;
+
+        const users = await convex.query(api.user.getUsers, {
+          email: user.email,
+        });
+
+        if (users.length === 0) {
+          await convex.mutation(api.user.createUser, {
+            name: user.given_name ?? '',
+            email: user.email,
+            image: user.picture ?? '',
+          });
+        }
+      } catch (error) {
+        console.error('Error while checking user:', error);
+      }
+    };
+
+    checkAndCreateUserIfNotExist();
+  }, [convex, user]);
+};
+
+export default useUserCreation;
