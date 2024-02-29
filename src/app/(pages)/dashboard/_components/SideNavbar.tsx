8d357c5b0f50fb383a5ce4a6f4945b401b501db7
Sabry Awad <dr.sabry97@gmail.com>
2024-02-29 23:16:37 +0200

    🔧 chore(SideNavbar.tsx): Update imports for dialog components 📝 docs(SideNavbar.tsx): Add missing comments and update variable names 🐛 fix(SideNavbar.tsx): Fix file creation logic and add missing import 🎉 feat(SideNavbar.tsx): Add upgrade plan dialog content ⚙️ chore(index.ts): Add constant for maximum free files

--- src/app/(pages)/dashboard/_components/SideNavbar.tsx
+++ src/app/(pages)/dashboard/_components/SideNavbar.tsx
@@ -1,7 +1,18 @@
 'use client';
 
+import { MAX_FREE_FILE } from '@/app/_constants';
 import { Button } from '@/components/ui/button';
-import { DialogHeader, DialogFooter } from '@/components/ui/dialog';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
 import {
   Popover,
   PopoverContent,
@@ -13,14 +24,6 @@ import { Team } from '@/convex/team';
 import { cn } from '@/lib/utils';
 import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs';
 import { KindeUser } from '@kinde-oss/kinde-auth-nextjs/dist/types';
-import {
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  DialogTitle,
-  DialogDescription,
-  DialogClose,
-} from '@radix-ui/react-dialog';
 import { useConvex } from 'convex/react';
 import {
   Archive,
@@ -33,8 +36,8 @@ import {
   Users,
 } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { Input } from 'postcss';
 import { useEffect, useState } from 'react';
 
 interface Props {
@@ -83,6 +86,8 @@ const SideNavbar: React.FC<Props> = ({ user }) => {
   const [teams, setTeams] = useState<Team[]>([]);
   const [activeTeam, setActiveTeam] = useState<Team | null>(null);
 
+  const [fileInput, setFileInput] = useState('');
+
   useEffect(() => {
     getTeamList();
   }, [user]);
@@ -102,6 +107,10 @@ const SideNavbar: React.FC<Props> = ({ user }) => {
     if (item.href) {
       nextRouter.push(item.href);
     }
+  };
+
+  const onFileCreate = (fileName: string) => {
+    console.log(fileName);
   };
 
   return (
@@ -218,6 +227,265 @@ const SideNavbar: React.FC<Props> = ({ user }) => {
               New File
             </Button>
           </DialogTrigger>
+
+          {MAX_FREE_FILE > 0 ? (
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Create New File</DialogTitle>
+                <DialogDescription>
+                  <Input
+                    placeholder="Enter File Name"
+                    className="mt-3"
+                    onChange={e => setFileInput(e.target.value)}
+                  />
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter className="">
+                <DialogClose asChild>
+                  <Button
+                    type="button"
+                    className="bg-blue-600 hover:bg-blue-700"
+                    disabled={!fileInput.length}
+                    onClick={() => onFileCreate(fileInput)}
+                  >
+                    Create
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          ) : (
+            <DialogContent className="max-w-4xl">
+              <DialogHeader>
+                <DialogTitle>Upgrade Plan</DialogTitle>
+                <DialogDescription>
+                  <div className="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8">
+                      <div className="rounded-2xl border border-indigo-600 p-6 shadow-sm ring-1 ring-indigo-600 sm:order-last sm:px-8 lg:p-12">
+                        <div className="text-center">
+                          <h2 className="text-lg font-medium text-gray-900">
+                            Professional
+                            <span className="sr-only">Plan</span>
+                          </h2>
+
+                          <p className="mt-2 sm:mt-4">
+                            <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
+                              4.99$
+                            </strong>
+
+                            <span className="text-sm font-medium text-gray-700">
+                              /month
+                            </span>
+                          </p>
+                        </div>
+
+                        <ul className="mt-6 space-y-2">
+                          <li className="flex items-center gap-1">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-5 text-indigo-700"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M4.5 12.75l6 6 9-13.5"
+                              />
+                            </svg>
+
+                            <span className="text-gray-700">
+                              Everything included in free, plus:
+                            </span>
+                          </li>
+
+                          <li className="flex items-center gap-1">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-5 text-indigo-700"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M4.5 12.75l6 6 9-13.5"
+                              />
+                            </svg>
+
+                            <span className="text-gray-700">
+                              Unlimited Team Files
+                            </span>
+                          </li>
+
+                          <li className="flex items-center gap-1">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-5 text-indigo-700"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M4.5 12.75l6 6 9-13.5"
+                              />
+                            </svg>
+
+                            <span className="text-gray-700">
+                              More document features
+                            </span>
+                          </li>
+
+                          <li className="flex items-center gap-1">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-5 text-indigo-700"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M4.5 12.75l6 6 9-13.5"
+                              />
+                            </svg>
+
+                            <span className="text-gray-700">Email Support</span>
+                          </li>
+
+                          <li className="flex items-center gap-1">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-5 text-indigo-700"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M4.5 12.75l6 6 9-13.5"
+                              />
+                            </svg>
+
+                            <span className="text-gray-700">
+                              Instagram support
+                            </span>
+                          </li>
+                        </ul>
+
+                        <Link
+                          href="#"
+                          className="mt-8 block rounded-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-indigo-700 hover:ring-1 hover:ring-indigo-700 focus:outline-none focus:ring active:text-indigo-500"
+                        >
+                          Upgrade
+                        </Link>
+                      </div>
+
+                      <div className="rounded-2xl border border-gray-200 p-6 shadow-sm sm:px-8 lg:p-12">
+                        <div className="text-center">
+                          <h2 className="text-lg font-medium text-gray-900">
+                            Free
+                            <span className="sr-only">Plan</span>
+                          </h2>
+
+                          <p className="mt-2 sm:mt-4">
+                            <strong className="text-3xl font-bold text-gray-900 sm:text-4xl">
+                              Free
+                            </strong>
+
+                            <span className="text-sm font-medium text-gray-700">
+                              /month
+                            </span>
+                          </p>
+                        </div>
+
+                        <ul className="mt-6 space-y-2">
+                          <li className="flex items-center gap-1">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-5 text-indigo-700"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M4.5 12.75l6 6 9-13.5"
+                              />
+                            </svg>
+
+                            <span className="text-gray-700">5 Team files</span>
+                          </li>
+
+                          <li className="flex items-center gap-1">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-5 text-indigo-700"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M4.5 12.75l6 6 9-13.5"
+                              />
+                            </svg>
+
+                            <span className="text-gray-700">
+                              Limited Document feature
+                            </span>
+                          </li>
+
+                          <li className="flex items-center gap-1">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="size-5 text-indigo-700"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M4.5 12.75l6 6 9-13.5"
+                              />
+                            </svg>
+
+                            <span className="text-gray-700">Email Support</span>
+                          </li>
+                        </ul>
+
+                        <a
+                          href="#"
+                          className="mt-8 block rounded-full border border-indigo-600 bg-white px-12 py-3 text-center text-sm font-medium text-indigo-600 hover:ring-1 hover:ring-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                        >
+                          Get Started
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter className="">
+                <DialogClose asChild></DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          )}
         </Dialog>
 
         <div className="mt-5 h-4 w-full rounded-full bg-gray-200">
