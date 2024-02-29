62ea8e3d565c3da9b2439c4a8c10168d6faf68a4
Sabry Awad <dr.sabry97@gmail.com>
2024-02-29 17:24:57 +0200

    🩹 chore(.prettierrc): Update Prettier configuration for Tailwind CSS plugin 📦 chore(package.json): Add Prettier Tailwind CSS plugin 🔧 chore(Hero.tsx): Refactor Hero component styling and fix class ordering

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -7,31 +7,27 @@ const Hero = () => {
         className="flex items-baseline 
         justify-center pt-20"
       >
-        <h2
-          className="text-white border 
-            px-3 p-2 rounded-full
-        text-center border-white"
-        >
+        <h2 className="rounded-full border border-white p-2 px-3 text-center text-white">
           See What's New | <span className="text-sky-300">AI Diagram</span>
         </h2>
       </div>
       <div className="mx-auto h-screen max-w-screen-xl px-4 py-12 lg:flex">
         <div className="mx-auto max-w-xl text-center">
-          <h1 className="text-3xl text-sky-300 font-extrabold sm:text-5xl">
+          <h1 className="text-3xl font-extrabold text-sky-300 sm:text-5xl">
             Documents & diagrams
             <strong className="font-extrabold text-white sm:block">
               for engineering teams.
             </strong>
           </h1>
 
-          <p className="mt-4 sm:text-xl/relaxed text-slate-200">
+          <p className="mt-4 text-slate-200 sm:text-xl/relaxed">
             All-in-one markdown editor, collaborative canvas, and
             diagram-as-code builder
           </p>
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
-              className="block w-full rounded bg-white text-black px-12 py-3 text-sm font-medium shadow hover:bg-slate-100 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-black shadow hover:bg-slate-100 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
               href="#"
             >
               Learn More
